9064d8ebadc9760e41f2346f7dedca95d10f3f28
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-28 16:05:18 -0700

    Initialize operand and operator variables and implement operate function

--- scripts.js
+++ scripts.js
@@ -7,3 +7,23 @@ function divide(a, b) {
         return "ERROR";
     } return a / b;
 }
+
+function operate(leftOperand, rightOperand, operator) {
+    switch (operator) {
+        case ("ADD") :
+            return add(leftOperand, rightOperand);
+        case ("SUBTRACT"):
+            return subtract(leftOperand, rightOperand);
+        case ("MULTIPLY"):
+            return multiply(leftOperand, rightOperand);
+        case ("DIVIDE"):
+            return divide(leftOperand, rightOperand);
+        default:
+            return "OPERATE FUNCTION ERROR";
+    }
+}
+
+// VARIABLES 
+let leftOperand;
+let rightOperand;
+let operator;
