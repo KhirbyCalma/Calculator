7aebe04443360eeda58b4c2927af1237a37f12eb
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-28 15:58:41 -0700

    Implement add, subtract, multiply, and divide functions

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+// FUNCTIONS
+function add(a, b) { return a + b; }
+function subtract(a, b) { return a - b; }
+function multiply(a, b) { return a * b; }
+function divide(a, b) {
+    if (b === 0) {
+        return "ERROR";
+    } return a / b;
+}
