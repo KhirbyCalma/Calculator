e68954c75acc8c8192db80abbdd16762ac5868b4
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-29 20:20:21 -0700

    Add setting operand and operator logic and made updating display function

--- scripts.js
+++ scripts.js
@@ -23,18 +23,46 @@ function operate(leftOperand, rightOperand, operator) {
     }
 }
 
+function setOperand(num) {
+    // meaning operator has not been set (meaning left operand needs to be filled)
+    if ( !operator ) {
+        leftOperand += num;
+    } 
+    // meaning operator has been set (meaning left operand is locked in)
+    else if (operator) {
+        rightOperand += num;
+    }
+}   
+
+function setOperator(op) {
+    // meaning there has to exist a left operand for an operator to be set
+    if (leftOperand) {
+        operator = op;
+    }
+}
+
+function updateDisplay() {
+    displayTextOutput.textContent = `${leftOperand} ${operator} ${rightOperand}`;
+}
+
 // VARIABLES 
-let leftOperand;
-let rightOperand;
-let operator;
+let leftOperand = '';
+let rightOperand = '';
+let operator = '';
 
 // QUERY SELECTORS
 const displayTextOutput = document.querySelector(".display .text-output");
 const numpadButtons = document.querySelectorAll(".numpad button.number");
 for (const numberButton of numpadButtons) {
-    numberButton.addEventListener("click", () => displayTextOutput.textContent += numberButton.textContent);
+    numberButton.addEventListener("click", () => {
+        setOperand(numberButton.textContent);
+        updateDisplay();
+});
 }
 const operatorButtons = document.querySelectorAll(".operators button.operator");
 for (const operatorButton of operatorButtons) {
-    operatorButton.addEventListener("click", () => displayTextOutput.textContent += operatorButton.textContent);
+    operatorButton.addEventListener("click", () => {
+        setOperator(operatorButton.textContent);
+        updateDisplay();
+    });
 }
